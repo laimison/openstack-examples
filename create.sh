a7b270819e370209e3b3f840e33a56899e991c65
laimison <laimison1@gmail.com>
2019-06-17 13:51:21 +0100

    VM creation script is simplified

--- create.sh
+++ create.sh
@@ -6,7 +6,7 @@ cat .env.sh | grep -v '^#'
 
 echo press enter; read
 
-if ! openstack server list 2>/dev/null >/dev/null; then echo "Please authenticate first"; exit 1; fi
+if ! openstack server list 2>/dev/null >/dev/null; then echo "Please authenticate first, an example: . ~/DEV-openrc.sh"; exit 1; fi
 
 echo
 echo "Will create VM in 8 seconds ..."
@@ -15,37 +15,24 @@ sleep 8
 # Disk
 openstack volume create --image $image --size $disk_size $host && \
 
-# App Network Card
-openstack port create --network $network_name --fixed-ip subnet="$subnet_name",ip-address=$ip $host && \
-port_id=`openstack port list | grep $ip | awk -F '|' '{print $2}'` && \
-openstack port show $port_id && \
-icmp=`openstack security group list | grep icmp_allowed | awk -F '|' '{print $2}'` && \
-echo $icmp && \
-ssh=`openstack security group list | grep ssh_allowed | awk -F '|' '{print $2}'` && \
-echo $ssh && \
-openstack port set --security-group $icmp $host && \
-openstack port set --security-group $ssh $host && \
-openstack port show $host && \
+# Variables
+icmp=`openstack security group list | grep icmp_allowed | awk -F '|' '{print $2}'`
+ssh=`openstack security group list | grep ssh_allowed | awk -F '|' '{print $2}'`
 
-echo press enter; read
+# App Interface
+openstack port create --network $network_name --fixed-ip subnet="$subnet_name",ip-address=$ip $host
+openstack port set --security-group $icmp $host
+openstack port set --security-group $ssh $host
+
+echo press enter to create VM; read
+
+openstack server create --flavor $flavor --security-group default --security-group ssh_allowed --security-group monitoring --security-group icmp_allowed --port $host --volume $host $host
+
+echo press enter if you want NFS interface; read
 
 # NFS Network Card
-openstack port create --network $network_name_nfs --fixed-ip subnet="$subnet_name_nfs",ip-address=$ip_nfs ${host}_nfs && \
-openstack port set --no-security-group --disable-port-security ${host}_nfs && \
-port_id=`openstack port list | grep $ip_nfs | awk -F '|' '{print $2}'` && \
-openstack port show $port_id
+openstack port create --network $network_name_nfs --fixed-ip subnet="$subnet_name_nfs",ip-address=$ip_nfs ${host}_nfs
+openstack port set --no-security-group --disable-port-security ${host}_nfs
+port_id=`openstack port list | grep $ip_nfs | awk -F ' ' '{print $2}'`
 
-echo press enter; read
-
-# Create VM
-if openstack port show $port_id >/dev/null 2>/dev/null
-then
-  openstack server create --flavor $flavor --security-group default --security-group ssh_allowed --security-group monitoring --security-group icmp_allowed --port $host --port ${host}_nfs --volume $host $host
-else
-  openstack server create --flavor $flavor --security-group default --security-group ssh_allowed --security-group monitoring --security-group icmp_allowed --port $host --volume $host $host
-fi
-
-echo $?
-
-# List VMs
-openstack server list
+nova interface-attach --port-id $port_id $host
